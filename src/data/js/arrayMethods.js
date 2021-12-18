554ec96292a2df4d38bb03542c8a3b6b79055719
Marek Gudalewicz <marek.gudalewicz@localhost>
2021-12-18 02:56:56 +0100

    Array methods content added

--- src/data/js/arrayMethods.js
+++ src/data/js/arrayMethods.js
@@ -223,9 +223,337 @@ arr.forEach(()=>{console.log('dummy')})
                         },
 
 
+                        {
+                            [Symbol('title')]:'indexOf',
+                            [Symbol('code')]:`                    
+<pre>
+let arr1 = [1,2,3,4,5,6];
+console.log(arr1.indexOf(3));// 2;
+console.log(arr1.indexOf(9));//-1;
+console.log(arr1.indexOf(6, 3)); //5
+</pre>                            
+                            
+                            `,
+                            Method: '<code>arr.indexOf(item, from)</code>',
+                            Arguments: `
+                            <ul>
+                                <li><code>item</code>: item to serach array for</li>
+                                <li><code>from</code>: index to start search from, optional</li>
+                            </ul>
+                            `,
+                            Returns: 'index of found element or -1 if nothing found',
+                            Mutating: 'Not mutating',
+                            Description: `Searches an array for a given element and returns -1 if nothing 
+                            found or found element index. Uses a '===' operator.`
+                        },
 
-                    ]
-                },
+                        {
+                            [Symbol('title')]:'lastIndexOf',
+                            [Symbol('code')]:`                    
+<pre>
+let arr1 = [1,2,3,4,5,6];
+console.log(arr1.lastIndexOf(3));// 2;
+console.log(arr1.lastIndexOf(9));//-1;
+console.log(arr1.lastIndexOf(6, 3));
+// -1 as starts from 3 element from the end and in this case 
+// does not find 6, as it is the first element form the end
+</pre>                            
+                            
+                            `,
+                            Method: '<code>arr.lastIndexOf(item, from)</code>',
+                            Arguments: `
+                            <ul>
+                                <li><code>item</code>: item to serach array for</li>
+                                <li><code>from</code>: index to start search from counting from the end of the array</li>
+                            </ul>
+                            `,
+                            Returns: 'index of found element or -1 if nothing found',
+                            Mutating: 'Not mutating',
+                            Description: `Searches an array starting from the end for a given element and returns 
+                            -1 if nothing found or found element index. Uses a '===' operator.`
+                        },
+
+
+
+
+                        {
+                            [Symbol('title')]:'includes',
+                            [Symbol('code')]:`                    
+<pre>
+let arr1 = [1,2,3,4,5,6];
+console.log(arr1.includes(3));// true;
+console.log(arr1.includes(9));// false;
+console.log(arr1.includes(6, 3)); // true;
+</pre>                            
+                            
+                            `,
+                            Method: '<code>arr.includes(item, from)</code>',
+                            Arguments: `
+                            <ul>
+                                <li><code>item</code>: item to serach array for</li>
+                                <li><code>from</code>: index to start search from counting from the end of the array</li>
+                            </ul>
+                            `,
+                            Returns: 'index of found element or -1 if nothing found',
+                            Mutating: 'Not mutating',
+                            Description: `Searches an array starting from the end for a given element and returns 
+                            -1 if nothing found or found element index. Uses a '===' operator.`
+                        },
+
+
+                        {
+                            [Symbol('title')]:'find',
+                            [Symbol('code')]:`                    
+<pre>
+let arr = [1,2,3,4,5,6];
+let found = arr.find((item, index, array)=>{
+    console.log(\`Searching item \${item} at \${index} of \${array.toString}\`);
+    return item % 3 == 0 ? true : false;
+})
+//Searching item 1 at 0 of 1,2,3,4,5,6
+//Searching item 2 at 1 of 1,2,3,4,5,6
+//Searching item 3 at 2 of 1,2,3,4,5,6
+console.log(found) //3;
+</pre>                            
+                            
+                            `,
+                            Method: '<code>arr.find((item, index, array))</code>',
+                            Arguments: `
+                            A callback function taking
+                            <ul>
+                                <li><code>item</code>: currently processed array item</li>
+                                <li><code>index</code>: index of currently processed item</li>
+                                <li><code>array</code>: given array</li>
+                            </ul>
+                            and returning <code>true</code> if element is found or <code>false</code>
+                            `,
+                            Returns: 'found element or <code>undefined</code> if nothing found',
+                            Mutating: 'Not mutating',
+                            Description: `Calls a callback function on each element of the given array, and 
+                            if callback returns true function stops and returns found element. If each callback
+                            execution returns false then <code>find</code> returns <code>undefined</code>`
+                        },
+
+
+
+
+                        {
+                            [Symbol('title')]:'findIndex',
+                            [Symbol('code')]:`                    
+<pre>
+let arr = [1,2,3,4,5,6];
+let found = arr.findIndex((item, index, array)=>{
+    console.log(\`Searching item \${item} at \${index} of \${array.toString}\`);
+    return item % 3 == 0 ? true : false;
+})
+//Searching item 1 at 0 of 1,2,3,4,5,6
+//Searching item 2 at 1 of 1,2,3,4,5,6
+//Searching item 3 at 2 of 1,2,3,4,5,6
+console.log(found) // 2;
+</pre>                            
+                            
+                            `,
+                            Method: '<code>arr.findIndex((item, index, array))</code>',
+                            Arguments: `
+                            A callback function taking
+                            <ul>
+                                <li><code>item</code>: currently processed array item</li>
+                                <li><code>index</code>: index of currently processed item</li>
+                                <li><code>array</code>: given array</li>
+                            </ul>
+                            and returning <code>true</code> if element is found or <code>false</code>
+                            `,
+                            Returns: 'found element or -1 if nothing found',
+                            Mutating: 'Not mutating',
+                            Description: `Calls a callback function on each element of the given array, and 
+                            if callback returns true function stops and returns an index of the found element. 
+                            If each callback
+                            execution returns false then <code>findIndex</code> returns <code>-1</code>`
+                        },
+
+
+                        {
+                            [Symbol('title')]:'filter',
+                            [Symbol('code')]:`                    
+<pre>
+let arr = [1,2,3,4,5,6];
+let found = arr.findIndex((item, index, array)=>{
+    console.log(\`Processing item \${item} at \${index} of \${array.toString}\`);
+    return item % 2 == 0 ? true : false;
+})
+//Processing item 1 at 0 of 1,2,3,4,5,6
+//Processing item 2 at 1 of 1,2,3,4,5,6
+//Processing item 3 at 2 of 1,2,3,4,5,6
+//Processing item 4 at 0 of 1,2,3,4,5,6
+//Processing item 5 at 1 of 1,2,3,4,5,6
+//Processing item 6 at 2 of 1,2,3,4,5,6
+console.log(found) // [2, 4, 6];
+</pre>                            
+                            
+                            `,
+                            Method: '<code>arr.filter((item, index, array))</code>',
+                            Arguments: `
+                            A callback function taking
+                            <ul>
+                                <li><code>item</code>: currently processed array item</li>
+                                <li><code>index</code>: index of currently processed item</li>
+                                <li><code>array</code>: given array</li>
+                            </ul>
+                            and returning <code>true</code> if element matches or <code>false</code>
+                            `,
+                            Returns: 'Array of found elements',
+                            Mutating: 'Not mutating',
+                            Description: `Calls a callback function for each array element. Returns an array of elements,
+                            for which the callback returned <code>true</code>`
+                        },
+
+
+                        {
+                            [Symbol('title')]:'map',
+                            [Symbol('code')]:`                    
+<pre>
+let arr = [1,2,3,4,5,6];
+let found = arr.map((item, index, array)=>{
+    return \<div>\${item}\</div>
+})
+console.log(found) 
+// ["&lt;div>1&lt;/div>", "&lt;div>2&lt;/div>", "&lt;div>3&lt;/div>", 
+// "&lt;div>4&lt;/div>", "&lt;div>5&lt;/div>", "&lt;div>6&lt;/div>"];
+</pre>                            
+                            
+                            `,
+                            Method: '<code>arr.map((item, index, array))</code>',
+                            Arguments: `
+                            A callback function taking
+                            <ul>
+                                <li><code>item</code>: currently processed array item</li>
+                                <li><code>index</code>: index of currently processed item</li>
+                                <li><code>array</code>: given array</li>
+                            </ul>
+                            and returning an item value for the new array
+                            `,
+                            Returns: 'Array of elements',
+                            Mutating: 'Not mutating',
+                            Description: `
+                            Calls a callback function for each element of the given array and returns a new array
+                            of elements returned by the callback function.
+                            `
+                        },
+
+
+
+                        {
+                            [Symbol('title')]:'sort',
+                            [Symbol('code')]:`                    
+        <pre>
+        let arr = [3,2,5,7,3];
+        let sorted = arr.sort((a,b)=>{
+            if (a<b) return -1;
+            if (a == b) return 0;
+            return 1
+        })
+        console.log(arr);
+        //[2, 3, 3, 5, 7]
+        console.log(sorted);
+        //[2, 3, 3, 5, 7]
+        
+        </pre>                            
+                            
+                            `,
+                            Method: '<code>arr.sort((a, b))</code>',
+                            Arguments: `
+                            A callback comparing function taking 2 elements that will be compared
+                            Callback should return:
+                            <ul>
+                                <li><code>-1</code>: if a > b</li>
+                                <li><code>0</code>: if a == b due to sorting criteria</li>
+                                <li><code>1</code>: if a < b</li>
+                            </ul>
+                            `,
+                            Returns: 'Array of elements',
+                            Mutating: 'Mutating',
+                            Description: `
+                            Sorts a given array, elements are compared by the callback function, the mutated, sorted array
+                            is returned.
+                            `
+                        },
+
+
+                        {
+                            [Symbol('title')]:'reverse',
+                            [Symbol('code')]:`                    
+        <pre>
+        let arr = [3,2,5,7,3];
+        let reversed = arr.reverse();
+        console.log(arr);
+        // [3,2,5,7,3]
+        console.log(reversed);
+        // [3,7,5,2,3]
+        console.log(Object.is(arr, reversed));
+        //true
+        
+        </pre>                            
+                            
+                            `,
+                            Method: '<code>arr.reverse()</code>',
+                            Arguments: `-`,
+                            Returns: 'Array of elements',
+                            Mutating: 'Mutating',
+                            Description: `
+                            Reverses the order of the elements in given array. Mutates the array.
+                            `
+                        },
+
+
+                        {
+                            [Symbol('title')]:'join',
+                            [Symbol('code')]:`                    
+<pre>
+let arr = [3,2,5,7,3];
+let joined = arr.join('=>');
+console.log(joined);
+// "3=>2=>5=>7=>3"
+</pre>                            
+                    
+                            `,
+                            Method: '<code>arr.join(conjunction)</code>',
+                            Arguments: `<code>conjunction</code>: a string element that serves as a glue, is pressed between
+                            array elements`,
+                            Returns: 'String',
+                            Mutating: 'Not mutating',
+                            Description: `
+                            Forms a string from array elements, pressing the <code>conjunction</code> between array elements.
+                            `
+                        },
+
+                        {
+                            [Symbol('title')]:'Array.isArray',
+                            [Symbol('code')]:`                    
+<pre>
+console.log(Array.isArray([2,3,4]));
+//true
+console.log(Array.isArray({a:1,b:2}));
+//false
+</pre>                            
+                    
+                            `,
+                            Method: '<code>Array.isArray(arr)</code>',
+                            Arguments: `<code>arr</code>: an object/primitive that needs to be compared`,
+                            Returns: 'Boolean',
+                            Mutating: 'Not mutating',
+                            Description: `
+                            A static method, true if <code>arr.constructor.name==="Array"</code>
+                            `
+                        },
+
+
+            ]
+        },
+
+
+
+        //     ]
+        // },
 
 
 
