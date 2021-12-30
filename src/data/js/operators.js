98506c93308210ab39c0182779e3537b0f5896f6
Marek Gudalewicz <marek.gudalewicz@localhost>
2021-12-30 03:34:09 +0100

    addition operator content added

--- src/data/js/operators.js
+++ src/data/js/operators.js
@@ -193,6 +193,8 @@ null != undefined; // false
 NaN === NaN; // false !!
 // isNaN() should be used
 
++0 === -0; // true;
+
 2 === 2; // true;
 2 === 1; // false;
 2 === '2' // false;
@@ -397,6 +399,7 @@ BigInt
 3n <= 3; // true;
 
 Strings
+'5n' <= 6; // false as '5n' is not converted into BigInt
 'abc' <= 'cde'; // true;
 'abc' <= 'abc'; // true;
 'abc' <= '0bc'; // false;
@@ -483,8 +486,101 @@ Arrays
 
 
 
+
+
+
+
             ]
         },
+
+
+
+
+
+
+
+        {
+            elementType:'Headline-3',
+            content:'Arithmetic operators'
+        },
+        {
+            elementType: 'Paragraph',
+            content: `
+                Arithmetic operators take left and right operand, perform an opperation and return the result. 
+                In case types of operands do not match, an attempt to convert operands to a number is being performed.
+                BigInt type will not be converted to a number by default, though <code>Number(2n)</code> works.
+                In general if there is no possiblity to convert not matching type to a number, there will be an error.
+                There is an exception, the addition operator <code>+</code> is capable of joining strings. Moreover
+                this operator will convert numbers to strings, so <code>5 + '5' === '55'</code>
+            `
+        },
+
+        {
+            elementType: 'ConditionalArray',
+            headItems: [
+                'Operator','Name','Usage','Description'
+            ],
+            content: [
+                {
+                    [Symbol('title')]:'+',
+                    [Symbol('code')]:`
+                    <ul>
+                        <li>Both numeric operands: returns a sum</li>
+                        <li>At least one operand is a string: converts second operand to string and reutrns a string</li>
+                        <li>undefined + number is NaN</li>
+                        <li>undefined + null is NaN</li>
+                        <li>null + number is the number</li>
+                        <li>In case of an object (and Array) and any other operand converts to a string, where 
+                        object conversion is '[object Object]'</li>
+                        <li>A BigInt will not be automaticly converted into a number, but will be converted to 
+                        a string if the second operand is a string</li>
+                    </ul>                   
+<pre>
+4 + 5 // 9
+4 + '5' // '45'
+
+<b>undefined and null</b>
+undefined + 5 // NaN
+undefined + null // NaN
+null + 5 // 5
+
+<b>Object</b>
+{a: 5} + 6; // 6, as equals to some block and a <code>+6</code> 
+// a number with an unary operator <code>+</code>
+({a: 8} + 4); // '[object Object]4'
+[4] + 3; // '43'
+[3] + [4]; // '44'
+
+<b>BigInt</b>
+2n + 5; // Error, bigint will not be converted
+Number(2n) + 5; // 7
+4n + '5'; // '45
+
+<b>String</b>
+'abc' + 'cde' = 'abccde'
+
+</pre>                            
+                    
+                    `,
+                    Operator: '<code>+</code>',
+                    Name: `Addition`,
+                    Usage: 'g() + f()',
+                    Description: `
+                    In case of two number type operands returns a sum in case of two string operands returnes a 
+                    concatenated string. In case of other types performes a conversion. See code section for more 
+                    (in case of a non mobile device click this row)
+                    `
+                },
+            ]
+        },
+
+
+
+
+
+
+
+
 
                 {
                     elementType:'Headline-3',
