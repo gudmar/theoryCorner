aa0782446197b950b749d4357a8ef7fce768ebe3
Marek Gudalewicz <marek.gudalewicz@localhost>
2022-01-13 01:57:02 +0100

    finally and catch added to the promisses

--- src/data/js/promises.js
+++ src/data/js/promises.js
@@ -406,6 +406,37 @@ executor is a function taking the resolve(someValue), and reject(reason) functio
                 },
 
 
+                {
+                    elementType:'SmallHeadline',
+                    content:'Promise states'
+                },
+                {
+                    elementType:'Paragraph',
+                    content:`The newly created promise is in <i>pending</i> state. Its result is <code>undefined</code>.
+                    It is said to be not settled, not fulfilled. Once the promise is resolved, its state is switched to <i>fulfilled</i>
+                    and result changes to <code>resolved</code>. If the promise is rejected, its state changes to <i>fulfilled</i>, and
+                    result to <code>rejected</code>. The promise in fulfilled state is said to be settled. A fulfilled promise can be
+                    either resolved or rejected. Once settled (fulfilled) promise cannot change its state or result.`
+                    // This paragraph SPELL CORRECT
+                },
+                {
+                    elementType: 'Image',
+                    name: 'promiseStates.png',
+                    alt: 'Promise states'
+                },
+
+                {
+                    elementType:'SmallHeadline',
+                    content:'Promise chains'
+                },
+                {
+                    elementType:'Paragraph',
+                    content:`The promisses may be chained thanks to the <code>then</code>, <code>catch</code> and <code>finally</code>
+                    methods. Each of those methods returns a promise, so even <code>finally</code> does not have to be at the 
+                    end of the promisse chain. The important thing is, that if the last chain link rejects, or throws an error, 
+                    there will be an uncaught error. Please examine promise methods for more details.`
+                },
+
 
 
                 {
@@ -419,8 +450,8 @@ executor is a function taking the resolve(someValue), and reject(reason) functio
                             [Symbol('code')]:`
                          <ul>
                             <li>It is important to remember, that a function that has no return, returns undefined, so in case
-                            callbacks do not return a promise, that would take time to resolve, it is imediately resolved with the 
-                            <code>undefined</code> value, causing the next <code>then</code> in chain to be launched imediatley</li>
+                            callbacks do not return a promise, that would take time to resolve, it is immediately resolved with the 
+                            <code>undefined</code> value, causing the next <code>then</code> in the chain to be launched imediatley</li>
                          </ul>
                          <b>Execution order: in case of resolving, first callback is executed,
 in case of rejection of the preceding function, the second callback will be executed,</b>
@@ -469,15 +500,6 @@ in case of rejection of the preceding function, the second callback will be exec
 })();
 // resolved | resolved | rejected | resolved | and the outcome is 'Resolved'
 // YES, then may be resolved event if previous outcomes were rejected.
-
-
-
-
-
-
-
-
-
             </pre>                    
                             `,
                             Method: '<code>Promise.prototype.then(onResolved[, onRejected])</code>',
@@ -488,8 +510,8 @@ in case of rejection of the preceding function, the second callback will be exec
                                     <li>If <code>onResolved</code> function is replaced with a value,
                                     then this value is replaced with an <i>identity</i> function, returning
                                     the value that the promise that <code>then</code> was attached to resolved to</li>
-                                    <li>If <code>onRejected</code> function is replaced with a value,
-                                    then this value is replaced with an <i>thrower</i> function, throwing an error it
+                                    <li>If an <code>onRejected</code> function is replaced with a value,
+                                    then this value is replaced with a <i>thrower</i> function, throwing an error it
                                     received as an argument</li>
                                     
                                 </ul>
@@ -497,14 +519,254 @@ in case of rejection of the preceding function, the second callback will be exec
                             Returns: `
                             Returns a <b>promise</b> resolving to the value that the <code>onResolved</code>, 
                             or to the <code>onReject</code> callback returns
-                             In particular if 
-                            the funcion does not return a thing, the promise resolving to undefined is returned.
+                             In particular, if 
+                            the function does not return a thing, the promise resolving to undefined is returned.
                             `,
                             Description: `
                              The function runs a <code>onResolved</code> or <code>onRejected</code> callback, depending on the 
                              outcome of the preceding promise. The returned value is the outcome of the executed callback.
                             `
                         },
+
+
+
+
+
+
+
+
+
+
+
+
+
+                        {
+                            [Symbol('title')]:'catch',
+                            [Symbol('code')]:`
+                            promise1 | catch          | final result
+                            resolved | not executed D | resolved
+                            error    | resolved       | resolved
+                            error    | rejected       | Error
+<ul>
+    <li>May be described as <code>promise1.then(undefined, onRejection)</code></li>
+   <li><code>catch</code> may be in the middle of the promise chain</li>
+   <li>If <code>catch</code> callback rejects (or throws an error) all chained <code>then</code>
+   functions will run their rejection scenario, until one of them <b>resolves</b></li>
+   <li><code>catch</code> should end the promise chain, because if the last promise rejects, there will be an
+   uncaught error</li>
+   <li>Last <code>catch</code> should end with resolve to avoid an error. Last catch should handle all possible 
+   errors</li>
+   <li>If an error is thrown by one of promise callback functions, this promise will be rejected</li>
+</ul>                                                        
+                        
+<b>Catch with preceding promise resolved</b>
+            <pre>
+(async function evaluate(){
+    let pOK = function(val){ 
+        return function() {console.log('Resolved'+val);return Promise.resolve(val)}
+    }
+    let pNOK = function(val){ 
+        return function(){console.log('Rejected'+val);return Promise.reject(val)}
+    }
+    let outcome = pOK(0)().then(pOK(1),pNOK(1)).then(pOK(2),pNOK(2)).catch(pOK(3))
+    console.log(await outcome)
+})();
+// Resolved0 
+// Resolved1 
+// Resolved2 
+// Resolved4
+// This will execute no matter the predecessor outcome
+// 4
+// Catch not executed, as everything before passed
+            </pre>          
+            
+            
+            <b>Catch with preceding promise rejected</b>
+            <pre>
+(async function evaluate(){
+    let pOK = function(val){ return function() {console.log('Resolved'+val);return Promise.resolve(val)}}
+    let pNOK = function(val){ return function(){console.log('Rejected'+val);return Promise.reject(val)}}
+        let outcome = pOK(0)()
+            .then(pNOK(1),pNOK(1))
+            .catch(pOK(2))
+            .then(pOK(3),pNOK(3))
+            .then(pOK(4),pNOK(4))
+            .finally(()=>{console.log('This will execute no matter the precedor outcome')})
+            console.log(await outcome)
+    })();   
+//Resolved0
+//Rejected1
+//Resolved2
+//Resolved3
+//Resolved4
+// This will execute no matter the predecessor outcome
+//4
+            </pre>    
+            
+            
+            <b>Catch with preceding promise rejected and catch rejected, no final catch</b>
+            There will an uncaught error occur, as it is needed to set <code>catch</code>
+            always at the end of the chain.
+            <pre>
+(async function evaluate(){
+    let pOK = function(val){ return function() {console.log('Resolved'+val);return Promise.resolve(val)}}
+    let pNOK = function(val){ return function(){console.log('Rejected'+val);return Promise.reject(val)}}
+        let outcome = pOK(0)()
+            .then(pNOK(1),pNOK(1))
+            .catch(pNOK(2))
+            .then(pOK(3),pNOK(3))
+            .then(pOK(4),pNOK(4))
+            // no catch here, error will appear
+            .finally(()=>{console.log('This will execute no matter the precedor outcome')})
+            console.log(await outcome)
+})();
+//Resolved0
+//Rejected1
+//Rejected2
+//Rejected3
+//Rejected4
+// This will execute no matter the predecessor outcome
+// Uncaught (in promise) 4
+            </pre>     
+
+            <b>A Rejecting catch with preceding promise throwing error</b>
+            <pre>
+(async function evaluate(){
+    let pOK = function(val){ return function() {console.log('Resolved'+val);return Promise.resolve(val)}}
+    let pNOK = function(val){ return function(){console.log('Rejected'+val);return Promise.reject(val)}}
+    let err = function(val){return function(){throw new Error(val); return Promise.resolve(val)}}
+        let outcome = pOK(0)()
+            .then(pOK(1),pNOK(1))
+            .then(err(3),pNOK(3))
+            .catch(pNOK(2))
+            .then(pOK(3),pNOK(3))
+            .then(pOK(4),pNOK(4))
+            // .catch(()=>{console.log('Catching')})
+            .finally(()=>{console.log('This will execute no matter the precedor outcome')})
+            console.log(await outcome)
+})();
+//Resolved0
+//Rejected1
+//Rejected2
+//Rejected3
+//Rejected4
+// This will execute no matter the predecessor outcome
+// Uncaught (in promise) 4
+
+
+
+            </pre>    
+                            `,
+                            Method: '<code>Promise.prototype.catch(onRejected)</code>',
+                            Arguments: `
+                            <code>onRejected(reason)</code> is a callback returning a promise. If this promise 
+                            fulfilles to the resolved value, then whole catch is concidered resolved, if the 
+                            callback throws an error, or the result promise fulfills to rejected, the whole 
+                            catch is concidered rejected. If the argument of the <code>catch</code> is a value, not a
+                            function, then catch passes preceding promise result to the next function in the
+                            chain, if available.
+                            `,
+                            Returns: `
+                            Returns a <b>promise</b> fulfilling to the value that the <code>onReject</code> callback 
+                            returns. In particular if 
+                            the funcion does not return a thing, the promise resolving to undefined is returned.
+                            `,
+                            Description: `
+                             The function runs a <code>onRejected</code> callback, if the preceding promise fulfilled to
+                             the rejected value, and returnes a promise fulfilling with the value returned by the 
+                             callback.
+                            If the preceding promise was resolved then <code>catch</code> has no 
+                             effect, and the stearing is passed to the next functions in the chain
+                            `
+                        },
+                        
+
+
+
+
+
+
+
+                        {
+                            [Symbol('title')]:'finally',
+                            [Symbol('code')]:`
+            <pre>
+<b>finally in theory does not to be the last in the chain</b>
+<pre>
+(async function evaluate(){
+    let pOK = function(val){ return function() {console.log('Resolved'+val);return Promise.resolve(val)}}
+    let pNOK = function(val){ return function(){console.log('Rejected'+val);return Promise.reject(val)}}
+    let err = function(val){return function(){throw new Error(val); return Promise.resolve(val)}}
+        let outcome = pOK(0)()
+            .then(pOK(1),pNOK(1))
+            .then(pOK(3),pNOK(3))
+            .catch(pNOK(2))
+            .then(pOK(3),pNOK(3))
+            .finally(pNOK('fin'))
+            .then(pOK(4),pNOK(4))
+            console.log(await outcome)
+    })();
+</pre>            
+<b>Will execute no matter the outcome of preceding promise result</b>
+<pre>
+(async function evaluate(){
+    let pOK = function(val){ return function() {console.log('Resolved'+val);return Promise.resolve(val)}}
+    let pNOK = function(val){ return function(){console.log('Rejected'+val);return Promise.reject(val)}}
+    let err = function(val){return function(){throw new Error(val); return Promise.resolve(val)}}
+    
+        let outcome0 = pOK(0)()
+            .then(pOK(0),pNOK(0))
+            .finally(pOK('fin0'))
+            // Resolved1
+            // Resolvedfin1
+    
+        let outcome1 = pOK(0)()
+            .then(pOK(1),pNOK(1))
+            .finally(pNOK('fin1'))
+            // Resolved1
+            // Uncaught error -> finally failed
+    
+        let outcome2 = pOK(0)()
+            .then(pNOK(2),pNOK(2))
+            .finally(pOK('fin2'))
+            // Rejected2
+            // Resolvedfin2
+            // Uncaught error -> no catch, rejection before finally,
+            // but finally is not for catching errors
+    
+        let outcome3 = pOK(0)()
+            .then(err(3),pNOK(3))
+            .finally(pOK('fin3'))
+            // Rejected3
+            // Resolvedfin3
+            // Uncaught error -> no catch, rejection (error thrown) before finally,
+            // but finally is not for catching errors
+    
+        console.log(await outcome1); // fin1
+        console.log(await outcome2); // fin2
+        console.log(await outcome3); // fin3
+            
+    })();
+</pre>
+                            `,
+                            Method: '<code>Promise.prototype.finally(onFinally)</code>',
+                            Arguments: `
+                            <code>onFinally()</code> will be run no matter the predecessor rejects or resolves. The 
+                            only condition for this callback to be executed is the predecessor promise finalization.
+                            `,
+                            Returns: `
+                            Returns a <b>promise</b> fulfilling to the value that the <code>onFinally</code> callback 
+                            returns. In particular, if 
+                            the function does not return a thing, the promise resolving to undefined is returned.
+                            `,
+                            Description: `
+                             The function runs a <code>onFinally</code> callback as soon as the preceding promise fulfills
+                             (settles). This method returns a promise, so does not have to be at the end of the promise chain.
+                             <b>This function is not for handling errors!</b>. In case there is an uncaught rejected promise,
+                             <code>finally</code> will not prevent an error.
+                            `
+                        },
+                        // Spelling of array checked so far
                     ]
                 },
                 
