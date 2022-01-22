4ba895d06f1a76d85940e412c2feaa816f47fb82
Marek Gudalewicz <marek.gudalewicz@localhost>
2022-01-22 02:22:36 +0100

    CWC spelling checked

--- src/data/js/customWebComponent.js
+++ src/data/js/customWebComponent.js
@@ -1,4 +1,4 @@
-
+// SPELL CHECKED, CORRECT!
 let data =     {
     summary: 'custom web components',
     title: 'Custom web components',
@@ -18,21 +18,21 @@ let data =     {
                     elementType: 'Paragraph',
                     content: `
                     Encapsulation: closing some functionalities in one class or function. Test this class, and be
-                    sure it works as expected. This is a powerful tool for keeping code clean. 
-                    One of approaches to encapsulation is building web components. They are pieces of code 
-                    having their view representation binded to some logic. This could be compared to html
-                    <code>input</code> button. We cannot access its internal, but it works good, its behavour 
+                    sure it works as expected. This is a powerful tool for keeping the code clean. 
+                    One of the approaches to encapsulation is building web components. They are pieces of code 
+                    having their view representation bound to some logic. This could be compared to html
+                    <code>input</code> button. We cannot access its internal, but it works good, its behavior 
                     may be changed with properties.
                     `
                 },
                 {
                     elementType: 'Paragraph',
                     content: `
-                    Pure JS makes it possible to close an component in a single class. It not only has a HTML
-                    sturcture and logic binded. It closes its CSS so it will not interfere with the rest pages 
-                    css. It may use something called a <strong>shadow DOM</code>, a part of DOM that is closed
-                    for the outsied word. Its internals can not be found with simple DOM searching methods, and
-                    its CSS is separated form the outside world. 
+                    Pure JS makes it possible to close a component in a single class. It not only has an HTML
+                    structure and logic bound. It closes its CSS so it will not interfere with the rest pages 
+                    CSS. It may use something called a <strong>shadow DOM</code>, a part of the DOM that is closed
+                    for the outsie word. Its internals can not be found with simple DOM searching methods, and
+                    its CSS is separated from the outside world. 
                     `
                 },
                 {
@@ -51,13 +51,13 @@ let data =     {
                 {
                     elementType: 'UnsignedList',
                     content: [
-                        `<code>CWC</code>: a Custom Web Component, technolodgy allowint the definition of such a component,`,
+                        `<code>CWC</code>: a Custom Web Component, technology allowing the definition of such a component,`,
                         `<code>host</code> or <code>CWC host</code>: an element that is registered with 
                         the <code>window.customElements.define</code> method, and is the host of the shadow-DOM,`,
                         `<code>shadow-DOM</code> the part of the DOM that is separated from the <q>light</q> DOM
-                        and cannot be easly accessed with the document.getElementById or document.querySelector methods.
-                        CSS inside shadow root does not impact outside world, and ths CSS from the outside world does
-                        not infuance the shadow DOM css,`
+                        and cannot be easily accessed with the document.getElementById or document.querySelector methods.
+                        CSS inside the shadow root does not impact outside world, and this CSS from the outside world does
+                        not infuence the shadow DOM CSS,`
                     ]
                 },
 
@@ -69,18 +69,18 @@ let data =     {
                 {
                     elementType:'UnsignedList',
                     content:[
-                        `Is a class that extends a <code>HTMLElement{}</code> or any build-in element like 
+                        `Is a class that extends an <code>HTMLElement{}</code> or any build-in element like 
                         <code>HTMLParagraphElement</code>, <code>HTMLDivElement</code>, 
                         <code>HTMLCanvasElement</code>, <code>HTMLBodyElement</code>, <code>HTMLFormElement</code>`,
                         `The custom web component needs to be registered in the <code>window.customElements</code> register
                         with the <code>window.customElements.define('element-tag', className[, {extends: 'p'}], where 
                         the extends takes the tag name of the element that is extended, if the web component does 
-                        extend a concrete element, not a <code>HTMLElement</code>`,
-                        `Has lifecycle methods, like <code>constructor</code>, <code>connectedCallback</code>,
+                        extend a concrete element, not an <code>HTMLElement</code>`,
+                        `Has lifecycle methods, like the <code>constructor</code>, <code>connectedCallback</code>,
                         <code>attributeChangedCallback(attrName, oldVal, newVal)</code>, <code>disconnectedCallback</code>,
                         <code>adoptedCallback</code>`,
                         `Can use a template with styling and HTML structure to describe the view of the component`,
-                        `Can use &lt;slot> elements to input some content from the outside world,`
+                        `Can use <code>&lt;slot></code> elements to input some content from the outside world,`
                     ]
                 },
 
@@ -99,7 +99,7 @@ let data =     {
                         `Initializing state,`, `setting up event listeners`, `Creating a shadow DOM`,
                         `The <code>return</code> should not be used except for an early return of <code>this</code>`,
                         `Actual work should be placed in the <code>connedtedCallback</code>`,
-                        `<code>document.open()</code> or <code>document.write()</code> should not be caled here`
+                        `<code>document.open()</code> or <code>document.write()</code> should not be called here`
                     ]
                 },
 
@@ -110,7 +110,7 @@ let data =     {
                 {
                     elementType:'UnsignedList',
                     content:[
-                        `Invoced every time the component is added to the DOM,`,
+                        `Invoked every time the component is added to the DOM,`,
                         `It is called if an element is moved to another parent with an <code>element.appendChild(newParent)</code>
                         method,`,
                         `Communication with server or web workers here,`,
@@ -118,8 +118,8 @@ let data =     {
                         `Rendering,`,
                         `attributes and children should not be used here, to match <code>createElement()</code> or
                         <code>createElementNS()</code>`,
-                        `The good place for accessing attributes passed in the HTML tag,`,
-                        `All operations referring to the DOM structure of created web element here, as DOM becomes
+                        `The best place for accessing attributes to passed in the HTML tag,`,
+                        `All operations referring to the DOM structure of creating web element here, as DOM becomes
                         available here,`
                     ]
                 },
@@ -136,8 +136,8 @@ let data =     {
                         `Free resources that will not be cleared automatically,`,
                         `Unsubscribe from the DOM events,`,
                         `Stop interval timers,`,
-                        `Unsubscribe, unregister callbacks from global application services,`,
-                        `This hook is not called if an user closes the tab`
+                        `Unsubscribe, unregistered callbacks for global application services,`,
+                        `This hook is not called if a user closes the tab`
                     ]
                 },
                 {
@@ -149,7 +149,7 @@ let data =     {
                     content:[
                         `Will be invoked each time a registered attribute (for example passed in the HTML) changes,`,
                         `Uses a <code>static get observed attributes</code> method to learn what attributes will be 
-                        observed. This method returns an array of attribs: <code>return ['prop1, 'prop2']</code>`,
+                        observed. This method returns an array of attributes: <code>return ['prop1, 'prop2']</code>`,
                         `BEWARE :). This should not be used together with a proxy or a mutation observer 
                         APIS, as a reaction to the same change will appear multiple times,`
                     ]
@@ -192,9 +192,9 @@ document.querySelector('#newParent').appendChild(document.adoptNode(elementToBeM
                     The content should be set in the <code>connectedCallback</code>. The best way is to use a 
                     <code>template</code> element. This element is rendered only once and is not visible when placed in the 
                     main HTML file. It may be a good practice to create a separate HTML file for the template,
-                    and separate for the logic, and place them in one (separate) folder, just like Angular does.
+                    and separate from the logic, and place them in one (separate) folder, just like Angular does.
                     The template may have a <code>style</code> element with the CSS.
-                    It is possible to add the template content inside a <code>shadowRoot</code> element.
+                    It is possible to add the template content in a <code>shadowRoot</code> element.
                     The shadow root should be attached in the constructor.
                     `
                 },
@@ -314,7 +314,7 @@ customElements.define('section-element', SectionElement)
                         themes to the shadow DOM.`,
                         `The shadow-DOM host elements may be styled from the outside (but no influence on the interiors of the shadow DOM),
                         with a normal class or tag selectors: <code>my-custom-web-component{position...}</code>`,
-                        `<code>custom-web-component:defined<code> will select a custom-web-component element if it is registered with the
+                        `<code>custom-web-component:defined</code> will select a custom-web-component element if it is registered with the
                         <code>customElements.define</code> method. This element may appear in the HTML, but may be defined later with CSS,`,
                         `<code>custom-web-component:not(:defined)</code> this selects a custom-web-component if it is not defined. This is 
                         a good solution for hiding elements until they are registered with the <code>customElements.define</code>`
@@ -334,7 +334,7 @@ customElements.define('section-element', SectionElement)
                     elementType:'Paragraph',
                     content:`
                         There are some antipattens and patterns. Here the proper usage of the lifecycle methods
-                        will not be repeted, but just some general rules regarding clean code will be reminded.
+                        will not be repeated, but just some general rules regarding clean code will be reminded.
                     `
                 },
                 {
@@ -344,17 +344,17 @@ customElements.define('section-element', SectionElement)
                         no CSS modifications to elements outside the host,`,
                         `CWC should accept inputs <b>only<b> with the attributes, or optionaly with a mediator pattern, 
                         no other modifications to the CWC from the outside world,`,
-                        `A component shuld never modify its attributes, so CWC should not modify its attributes,`,
+                        `A component should never modify its attributes, so CWC should not modify its attributes,`,
                         `The proper way to communicate from CWC to the outside world is with the events dispatched on
-                        the host element. Optionaly the mediator pattern may be used,`,
+                        the host element. Optionally the mediator pattern may be used,`,
                         `The CWC host CSS should not be manipulated from the outside, as it kills the encapsulation.
-                        Thie execptions could be to hide the CWC,`,
+                        The exceptions could be to hide the CWC,`,
                         `Before using an attribute, check if it does not already exist, do not override,`,
                         `Before using a method name check if a property with the same name does not exist, do not override,`,
-                        `A comopnent should never apply classes to its self, so do not apply classes to a CWC element 
+                        `A component should never apply classes to its self, so do not apply classes to a CWC element 
                         from the inside,`,
                         `Methods and variable names should be descriptive,`,
-                        `Comments in the code should be newer used, code should speak of its self, commens get outdated and it
+                        `Comments in the code should be newer used, code should speak of its self, comments get outdated and it
                         is difficult to track if they are still up to date,`,
                         `Methods short, doing one thing,`,
                         `No <i>if</i> or <i>or</i> or <i>and</i> used in method naming, as those tell that a method does more 
