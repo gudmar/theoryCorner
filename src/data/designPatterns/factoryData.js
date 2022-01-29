6e5c7e4e81db2f95e962eae2e86f48335de6b657
Marek Gudalewicz <marek.gudalewicz@localhost>
2022-01-30 00:34:54 +0100

    prototype design pattern data added

---
+++ src/data/designPatterns/factoryData.js
@@ -0,0 +1,71 @@
+// SPELL CHECKED, CORRECT!
+let data =     {
+    summary: 'factory design pattern',
+    title: 'factory design pattern',
+    searchKeywords:`
+    `,
+    cathegory: 'DesignPatterns',
+    content: [
+        {
+            elementType:'Article',
+            content:[
+                {
+                    elementType:'Headline',
+                    content:'Prototype'
+                },
+
+                {
+                    elementType: 'Paragraph',
+                    content: `
+                    `
+                },
+
+
+                {
+                    elementType:'SmallHeadline',
+                    content:'Description'
+                },
+                {
+                    elementType:'SmallHeadline',
+                    content:'Understanding'
+                },
+                {
+                    elementType:'SmallHeadline',
+                    content:'Examples'
+                },
+                {
+                    elementType:'SmallHeadline',
+                    content:'Code'
+                },
+
+
+
+            ]
+        },
+
+        {
+            elementType:'Article',
+            content:[
+                {
+                    elementType:'Headline-2',
+                    content:'References'
+                },
+                {
+                    elementType:'Link',
+                    content:'refactoring guru',
+                    href: 'https://refactoring.guru/pl',
+                    description:'Tutorial'
+                },
+                {
+                    elementType:'Paragraph',
+                    content:'<i>Design patterns</i> by Erich Gamma, Richard Helm, Ralph Johnson, John Vlissides'
+                },
+                
+            ]
+        }
+    ]
+};
+
+export default function getFactoryData(){
+    return data;
+}
---
+++ src/data/theoryDataDesignPatterns.js
@@ -0,0 +1,55 @@
+// SPELL CHECKED, CORRECT!
+import getPrototypeData from "./designPatterns/prototypeData";
+import getFactoryData from "./designPatterns/factoryData";
+import getBridgeData from "./designPatterns/bridgeData";
+import getAbstractFactoryData from "./designPatterns/abstractFactoryData";
+
+
+
+let theoryDataDesignPatterns = [
+
+    getPrototypeData(),
+    getFactoryData(),
+    getAbstractFactoryData(),
+    getBridgeData(),
+    
+    // {
+    //     summary: 'Design Patterns',
+    //     title: 'design patterns',
+    //     searchKeywords: 'prototype abstract factory singleton builder bridge adapter flyweight iterator composite',
+    //     cathegory: 'DesignPatterns',
+    //     content: [
+    //         {
+    //             elementType:'Article',
+    //             content:[
+    //                 {
+    //                     elementType:'Headline',
+    //                     content:'Design patterns'
+    //                 },
+    //                 {
+    //                     elementType: 'Paragraph',
+    //                     content: `
+    //                         Design patterns are some typical solutions for problems met while inventing, designing 
+    //                         something. Design patterns may not be only programming oriented, as the same term 
+    //                         may be used when designing for example  traffic.
+    //                     `
+    //                 },
+    //                 {
+    //                     elementType: 'Paragraph',
+    //                     content: `
+    //                     In programming they are architectural, language agnostic solutions to common met problems. 
+    //                     It is good to know and use them, as more application is thought of in the early stage, 
+    //                     before implementation starts, the better for future maintainability. 
+    //                     `
+    //                 }
+
+    //             ]
+    //         },
+    //     ]
+    // },
+
+]
+
+export function getTheoryDataDesignPatterns(){
+    return theoryDataDesignPatterns;
+}
