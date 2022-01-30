1042ce6f74b64b4b174143b9f805e9b60a09c749
Marek Gudalewicz <marek.gudalewicz@localhost>
2022-01-30 02:14:35 +0100

    bridge pattern short description added

--- src/data/designPatterns/bridgeData.js
+++ src/data/designPatterns/bridgeData.js
@@ -11,31 +11,58 @@ let data =     {
             content:[
                 {
                     elementType:'Headline',
-                    content:'Prototype'
+                    content:'Bridge'
                 },
 
                 {
                     elementType: 'Paragraph',
                     content: `
+                    A structural design pattern. Means it shows a way to combine objects into bigger structures without
+                    losing flexibility. This pattern is similar to an abstract factory object in a way. It is abstract in
+                    the same way. It will not create an object, but will provide an abstract interface to use an object 
+                    of the certain type. Consider a car. No matter what car you are driving, most of them have 
+                    similar interfaces. So if you drove one, you most probably be able to use more of them.
                     `
                 },
 
-
                 {
-                    elementType:'SmallHeadline',
-                    content:'Description'
-                },
-                {
-                    elementType:'SmallHeadline',
+                    elementType:'Headline-2',
                     content:'Understanding'
                 },
                 {
-                    elementType:'SmallHeadline',
-                    content:'Examples'
+                    elementType:'Headline-3',
+                    content:'Paint application'
                 },
                 {
-                    elementType:'SmallHeadline',
-                    content:'Code'
+                    elementType:'Paragraph',
+                    content:`
+                    A paint application can draw: circles, triangles, lines, rectangles and paths. All those 
+                    objects are figures, and have a lot of functionalities in common. A figure will have below methods:
+                    `
+                },
+                {
+                    elementType:'UnsignedList',
+                    content:[
+                        `<code>draw</code>`, `<code>redraw</code> in case it is resized, old figure must be erased, and 
+                        new will be created,`,
+                        `<code>delete</code> used by the redraw,`, `<code>changeFill</code>`, `<code>changeStroke</code>`
+                    ]
+                },
+                {
+                    elementType:'Paragraph',
+                    content:`
+                    All those methods will be similar to all shapes. The path will have to have redraw altered,
+                    it will not delete older objects, just add next points to the path, but this is nothing 
+                    to worry about while implementing a bridge pattern, as bridge itself is an abstract 
+                    class, not knowing the implementation details.
+                    `
+                },
+                {
+                    elementType:'Paragraph',
+                    content:`
+                    Now more figures may be added only by creating the implementation. No need to 
+                    change the legacy code.
+                    `
                 },
 
 
