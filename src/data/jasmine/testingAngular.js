2fa5fdc910f6480f0b1636d97c7fdf9acb317fd2
Marek Gudalewicz <marek.gudalewicz@localhost>
2022-02-13 14:20:05 +0100

    content added to jasmine testing Angular

--- src/data/jasmine/testingAngular.js
+++ src/data/jasmine/testingAngular.js
@@ -291,6 +291,72 @@ it('should find <div> with fixture.debugElement.query(By.css)', () => {
                                             },
 
 
+                                            {
+                                                type: 'Item',
+                                                title: 'fixture.componentInstance',
+                                                description: {
+                                                    elementType:'Section',
+                                                    content:[
+                                                        {
+                                                            elementType: 'Paragraph',
+                                                            content: `
+                                                            <code>fixture.nativeElement</code> provides access to DOM, and
+                                                            <code>fixture.componentInstance</code> 
+                                                            gives access to the component internals. This makes it possible 
+                                                            to call lifecycle methods on the component and get to 
+                                                            internal component variables:
+
+                                                            `
+                                                        },
+                                                        {
+                                                            elementType: 'Paragraph',
+                                                            content: `
+                                                            Without dependencies, fixture might not be needed
+
+                                                            `
+                                                        },
+                                                        {
+                                                            elementType: 'Code',
+                                                            content: `
+                                                            <div class="note">Firstly lets declare the fixture and the 
+                                                            component</div>
+                                                            <pre>
+describe('',()=>{
+    let component: MyTestedClass = new MyTestedClass();
+})                                                            
+                                                            </pre>
+                                                            <div class="note">Now for the it block</div>
+<pre>
+    it('should pass', ()=>{
+        expect(component.someProp).toBe('');
+        component.onInit
+    })
+</pre>                                                            
+                                                            `
+                                                        },
+                                                        {
+                                                            elementType: 'Paragraph',
+                                                            content: `
+                                                            There is a way to get to the desired element with a <code>By.css</code>
+                                                            api. This is useful in case of non browser environments, where 
+                                                            is no DOM (like a server).
+                                                            `
+                                                        },
+                                                        {
+                                                            elementType: 'Code',
+                                                            content: `
+                                                            <pre>
+
+    
+                                                            </pre>
+                                                            `
+                                                        },
+    
+                                                        ]
+                                                    }
+                                                },
+
+
 
                                         ]
                                     }
@@ -298,6 +364,313 @@ it('should find <div> with fixture.debugElement.query(By.css)', () => {
                             ]
                         },
 
+        {
+            elementType:'Article',
+            content:[
+                {
+                    elementType:'Headline-2',
+                    content:'Testing a component'
+                },
+                {
+                    elementType:'Paragraph',
+                    content:`
+                    Entity tests should test elements in isolation. This meens, that a component should be tested 
+                    on its own. Testing a component is not testing related services, directives or pipes. 
+                    Everything has to be stubbed or mocked. If there is a service fetching data from the 
+                    server, the server communication is not allowed in ET. The service should be mocked with
+                    a class having methods named in the same way, and returning hardcoded values. 
+                    If a mocked service contains too much logic, then bugs may occure in the mocked service, and
+                    this is unwanted.
+                    `
+                },
+                {
+                    elementType:'Headline-2',
+                    content:'No dependencies'
+                },
+                {
+                    elementType:'Paragraph',
+                    content:`
+                    Let's take a simple component for exercising
+                    `
+                },
+                {
+                    elementType:'Code',
+                    content:`
+<div class="note">HTML</div>                    
+<pre>
+&lt;div class="alert alert-warning">
+    {{content}}
+&lt;/div>
+</pre>
+<div class="note">ts</div>                    
+<pre>
+@Component({
+    selector: 'my-component',
+    templateUrl: '...',
+    styleUrls: ['...']
+  })
+export class MyComponent implements OnInit {
+
+    constructor() { }
+    content: string = '';
+    ngOnInit(): void {
+      this.content = "This content was set by the ngOnInit"
+    }
+  }
+</pre>
+                    `
+                },
+
+                {
+                    elementType:'Paragraph',
+                    content:`
+                    The easiest way to create and test a component class is with the <code>new</code> operator
+                    usage:
+                    `
+                },
+                {
+                    elementType:'Code',
+                    content:`
+<pre>
+fdescribe('Setting test with new operator', ()=>{
+    let component = new MyComponent();
+    it('should be filled with data onInit',()=>{
+      expect(component.content).toBe('');
+      component.ngOnInit();
+      console.info('COMPONENT', component)
+      expect(component.content.length).toBeGreaterThan(4);
+    })
+  })
+</pre>
+                    `
+                },
+                {
+                    elementType:'Paragraph',
+                    content:`
+                    Above code allows to test the component class, even to invoke lifecycle methods on it.
+                    However, the Angular component consists of the ts class, that is binded with the HTML
+                    template. When the component is tested, the bindings should be tested. 
+                    So access to the DOM is needed, and the <code>detectChanges</code> method would be necessary.
+                    Because of this, the fixture is inevitable.
+                    `
+                },
+                {
+                    elementType:'Code',
+                    content:`
+<pre>
+describe('Test with the fixture', () => {
+    let component: MyComponent;
+    let fixture: ComponentFixture<MyComponent>;
+  
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        declarations: [ MyComponent ]
+      })
+      .compileComponents();
+    });
+  
+    beforeEach(() => {
+      fixture = TestBed.createComponent(MyComponent);
+      component = fixture.componentInstance;
+    });
+  
+    it('should fill the content property after ngOnInit', () => {
+      let domElement = fixture.debugElement.nativeElement.querySelector('div');
+      expect(component.content).toBe('');
+      // empty, as ngOnInit has to be invoked manualy
+      component.ngOnInit();
+      expect(component.content.length).toBeGreaterThan(5);
+      // after ngOnInit content has to be not empty
+      expect(domElement.innerHTML).toBe('');
+      // innerHTML is still empty, as changes are not detected automatically
+      fixture.detectChanges();
+      // After detecting chagnges, DOM content should be updated
+      expect(domElement.innerHTML).not.toBe('');
+    });
+  });
+</pre>
+                    `
+                },
+
+                {
+                    elementType:'SmallHeadline',
+                    content:'Testing a component with the service dependency'
+                },
+                {
+                    elementType:'Paragraph',
+                    content:`Lets take a simle component having a headline and a paragraph. Both values are
+                    set in ngOnInit by an async service:`
+                },
+                {
+                    elementType:'Code',
+                    content:`
+                    <div class="note">The service giving random strings</div>
+<pre>
+export class RandomDataProviderService {
+    getData(len:number){
+      let output = '';
+      let count = 0;
+      return new Promise((resolve)=>{
+        let int = setInterval(()=>{
+          let d = Date.now() * Math.random();
+          output += ` ` + d.toString(32);
+          count++;
+          if (count >= len) {
+            clearTimeout(int);
+            resolve(output);
+          }
+        },1)  
+      })
+    }
+  }
+</pre>               
+                <div class="note">And the components HTML</div>
+                <pre>                
+&lt;h5>{{headline}}&lt;/h5>
+&lt;p>{{content}}&lt;/p>
+&lt;button class="btn btn-info" (click)="update()">Update&lt;/button>
+                </pre>
+                <div class="note">And components ts</div>     
+<pre>
+export class LoadChangeContentComponent implements OnInit {
+    headline:string = '';
+    content:string = '';
+    constructor(private dataProvider: RandomDataProviderService) { }
+  
+    ngOnInit(): void {
+      this.setNewContent();
+    }
+  
+    setNewContent(){
+      let that = this;
+      this.dataProvider.getData(2).
+            then(function(value){that.headline = <string>value});;
+      this.dataProvider.getData(80).
+            then(function(value){that.content = <string>value});;
+    }
+  
+    update(){
+      console.log('Button clicked')
+      this.setNewContent();
+    }
+  }
+</pre>                
+                    `
+                },
+
+                {
+                    elementType:'Paragraph',
+                    content:`The service gives random values. For tests it is better to have 
+                    deterministic values. Service class has to be mocked:`
+                },
+                {
+                    elementType:'Code',
+                    content:`
+                    <div class="note">Mocked service</div>
+<pre>
+class MockRandomDataService {
+    nrOfRuns: number = 0;
+    getData(nr:number){
+      let output = '';
+      nr = 5 + this.nrOfRuns;
+      for (let i = 0; i < nr; i++){
+        output += i.toString(32);
+      }
+      return new Promise((resolve)=>{
+        setTimeout(()=>{
+          console.log(output)
+          resolve(output);
+        },5)
+      })
+    }
+  
+    increaseCounter(){
+      this.nrOfRuns++;
+    }
+  }
+</pre>`
+                },
+                {
+                    elementType:'Paragraph',
+                    content:`The <code>increaseCounter()</code> method will manualy change the data that the 
+                    mock is providing, but in a deterministic way. We are testing a component, not the service!`
+                },
+                
+                {
+                    elementType:'Paragraph',
+                    content:`Now for the tests`
+                },
+                {
+                    elementType:'Code',
+                    content:`
+<pre>
+describe('LoadChangeContentComponent', () => {
+    let component: LoadChangeContentComponent;
+    let fixture: ComponentFixture<LoadChangeContentComponent>;
+  
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        declarations: [ LoadChangeContentComponent ],
+        providers: [{provide: RandomDataProviderService, useClass: MockRandomDataService}]
+      })
+      .compileComponents();
+    });
+  
+    beforeEach(() => {
+      fixture = TestBed.createComponent(LoadChangeContentComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  
+    it('should component be empty before onInit', ()=>{
+       let p = fixture.nativeElement.querySelector('p');
+       let h = fixture.debugElement.nativeElement.querySelector('h5');
+       expect(p.innerText).toBe('');
+       expect(h.innerText).toBe('');
+    })
+  
+    it('should have content after onInit and promise resolve', fakeAsync(()=>{
+      let p = fixture.nativeElement.querySelector('p');
+      let h = fixture.debugElement.nativeElement.querySelector('h5');
+      component.ngOnInit();
+      tick(80);
+      fixture.detectChanges();
+      console.log(p)
+      expect(p.innerText).toBe('01234');
+      expect(h.innerText).toBe('01234');
+   }))
+  
+   it('should fill headline with data after ngOnInit', fakeAsync(()=>{
+     expect(component.headline).toBe('');
+     component.ngOnInit();
+     tick(80);
+     expect(component.headline).toBe('01234');
+   }))
+  
+   it('should update value after click', fakeAsync(()=>{
+    let p = fixture.nativeElement.querySelector('p');
+    let h = fixture.debugElement.nativeElement.querySelector('h5');
+    let b = fixture.nativeElement.querySelector('button');
+    let serv = TestBed.get(RandomDataProviderService);
+    component.ngOnInit();
+    tick(80);
+    fixture.detectChanges();
+    expect(p.innerText).toBe('01234');
+    expect(h.innerText).toBe('01234');
+    serv.increaseCounter();
+    b.click();
+    tick(80);
+    fixture.detectChanges();
+    expect(p.innerText).toBe('012345');
+    expect(h.innerText).toBe('012345');
+  }))
+  });
+</pre>                    
+                    `
+                },
+
+            ]
+        },
         {
             elementType:'Article',
             content:[
