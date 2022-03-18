1b27d70841b8d5d8801d699c88a62cd3746a90ec
Marek Gudalewicz <marek.gudalewicz@localhost>
2022-03-18 02:46:53 +0100

    grid-area some content added, photo bg stale added

--- src/data/css/grid.js
+++ src/data/css/grid.js
@@ -2,6 +2,7 @@ import elementGenerator from '../../services/elementGenerator'
 import getGridTemplateColumnContent from './gridFiles/gridTemplateColumn'
 import getGridTemplateRowContent from './gridFiles/gridTemplateRow'
 import getGridTemplateRowColumnContent from './gridFiles/girdTemplateRowAndColumn'
+import getGridTemplateAreaContent from './gridFiles/gridTemplateArea'
 
 let data =     {
     summary: 'Grid',
@@ -26,6 +27,7 @@ let data =     {
                         {id: 'template-rows', title: 'grid-template-rows'},
                         {id: 'template-rows-columns', title: 'grid-template-rows with grid-template-columns'},
                         {id: 'template-areas', title: 'grid-template-areas'},
+                        {id: 'grid-area', title: 'grid-area'},
                         {id: 'template', title: 'grid-template'},
                         {id: 'await', title: 'Async with <code>await</code>'},
                     ]
@@ -229,6 +231,182 @@ let data =     {
                     content:'<span id="template-rows-columns">grid-template-rows with grid-tempate-columns</span>'
                 },
                 {...getGridTemplateRowColumnContent()},
+
+                {
+                    elementType:'Headline-3',
+                    content:'<span id="template-areas">grid-template-area</span>'
+                },
+
+                {
+                    elementType: 'UnsignedList',
+                    content: [
+                        `<code>grid-template-area</code>: defines areas in a grid parent. Does position elements, but
+                        this template may be referred to from the child elements,`,
+                        `<code>grid-area</code>: a property of a child element, that defines to what part of the 
+                        <code>grid-template-area</code> element will belong,`,
+                        `Layout of the page may be changed by just change of the <code>grid-template-area</code>,`,
+                        `A <code>grid-template-area</code> consists of a set of strings, each of them defines a 
+                        single row of the temlate`,
+                        `A grid area is a part of the grid layout surrounded by 2 grid block lines and 2 grid row lines,
+                        so it will always be a rectangle of some sort`,
+                    ]
+                    
+                },
+
+                {...getGridTemplateAreaContent()},
+
+
+                {
+                    elementType:'Headline-3',
+                    content:'<span id="grid-area">grid-area</span>'                    
+                },
+                {
+                    elementType:'Paragraph',
+                    content:`
+                    <code>grid-area</code> can be one of below:
+                    `  
+                },
+                {
+                    elementType:'UnsignedList',
+                    content:[
+                        `<code>grid-area: area-name</code>: where <code>area-name</code> is specified 
+                        in the <code>grid-template-area</code>. In this case the element will expand to the 
+                        whole area that was defined with the <code>area-name</code> in the <code>grid-template-area</code>,`,
+                        `<code>grid-area: grid-row-start / grid-column-start / grid-row-end / grid-column-end</code>, 
+                        where <code>grid-row-start</code>, <code>grid-column-start</code>, <code>grid-row-end</code>,
+                        <code>grid-column-end</code> are numbers indicating the grid line numbers, or are names given to the 
+                        grid lines (eg. <code>grid-template-column: [someName] 1fr [someEndName]</code>,`,
+                        `The <code>grid-template-area</code> does not have to be defined for the <code>grid-area</code> to
+                        work. Only in this case <code>grid-area</code> property will have to be defined by the 
+                        <code>grid-row-start</code>, <code>grid-column-start</code>, <code>grid-row-end</code>,
+                        <code>grid-column-end</code> pattern,`,
+                        `A dot <code>.</code> in the <code>grid-template-area</code> indicates the area not taken by any
+                        element, the element the is not occupied at all. However this refers to the <code>grid-area: area-name</code>
+                        pattern only. If the some child of a grid container is place with the <code>grid-row-start</code>, <code>grid-column-start</code>, <code>grid-row-end</code>,
+                        <code>grid-column-end</code> pattern, it may be placed in the <i><q>forbidden zone</q></i>,`
+                    ]
+                },
+
+                {
+                    elementType:'SmallHeadline',
+                    content:`
+                        grid-area: image stale example
+                    `
+                },
+
+
+                {
+                    elementType: `exampleInIframe`,
+                    title: 'Slot example',
+                    src: 'https://gudmar.github.io/theoryCornerExamples/Grid/stalePhotoGrid.html',
+                    width: '500',
+                    height: '400'
+                },
+
+                {
+                    elementType:'Code',
+                    content:`
+<pre>
+&lt;style>
+html{
+    overflow: auto;
+}
+body{
+    padding:0;
+    margin:0;
+    background-image: url('./staleBG.png');
+    overflow: auto;
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+}
+.grid-parent{
+    width: 100vw;
+    position: relative;
+    display: grid;
+    grid-template-areas: "a a a a a"
+                         "b . . . c"
+                         "d . e . f"
+                         "g . . . h"
+                         "i i i i i";
+}
+.grid-child-a{
+    grid-area: a;
+    background-color: #777;
+    position: relative;
+    height: 20vh;
+}
+.grid-child-b{
+    grid-area: b;
+    background-color: #999;
+    height: 10vh;
+}
+.grid-child-c{
+    grid-area: c;
+    background-color: #999;
+}
+.grid-child-d{
+    grid-area: d;
+    background-color: #ddd;
+}
+.grid-child-e{
+    grid-area: e;
+    background-color: green;
+    height: 10vh;
+}
+.grid-child-f{
+    grid-area: f;
+    background-color: #ddd;
+}
+.grid-child-g{
+    grid-area: g;
+    background-color: #ddd;
+    height: 10vh;
+}
+.grid-child-h{
+    grid-area: h;
+    background-color: #ddd;
+}
+.grid-child-i{
+    grid-area: i;
+    background-color: #444;
+}
+
+.child-of-grid-i{
+    position: relative;
+    height: 100vh;
+}
+.center{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* align-content: center; */
+}
+.text{
+    font-family: Arial, Helvetica, sans-serif;
+    color: white;
+}
+&lt;/style>
+
+&lt;div class="grid-parent">
+&lt;div class="grid-child-a center text">&lt;h1>Lawnmower&lt;/h1>&lt;/div>
+&lt;div class="grid-child-b center text">b&lt;/div>
+&lt;div class="grid-child-c center text">c&lt;/div>
+&lt;div class="grid-child-f center text">f&lt;/div>
+&lt;div class="grid-child-g center text">g&lt;/div>
+&lt;div class="grid-child-h center text">h&lt;/div>
+&lt;div class="grid-child-i center text">
+    &lt;div class="child-of-grid-i center text">e-child&lt;/div>
+&lt;/div>
+&lt;div class="grid-child-d center text">d&lt;/div>
+&lt;div class="grid-child-e center text">e&lt;/div>
+&lt;/div>
+
+</pre>                    
+
+                    `
+                },
+
 
             ]
         },
---
+++ src/data/css/gridFiles/gridTemplateArea.js
@@ -0,0 +1,98 @@
+
+
+
+const gridTemplateAreaContent =     {
+    elementType: 'HiddenDescription',
+    content: {
+        legend: [
+            {
+                type: 'Example',
+                styleClasses: 'btn btn-secondary text-white m-4 mb-1 mt-1 hidden',
+                description: 'Example of grid usage'
+            },
+        ],
+        items:[
+            {
+                type: 'Example',
+                title: 'Area',
+
+                maxHeight: '60rem',
+                description: {
+                    elementType:'Section',
+                    content:[
+                            {
+                                elementType:'UnsignedList',
+                                content:[
+
+                                ]
+                            },
+                            {
+                                elementType:'Code',
+                                content:`
+<pre>
+<pre>                            
+                                `
+                            },
+                            {
+                                elementType:'GridPresent',
+                                parentWidth: undefined,
+                                parentHeight: 700,
+                                nrOfChildren: 10,
+                                parentStyle: {
+                                    gridTemplateAreas: `
+                                    "a a b b . . . d e"
+                                    "c c b b . . . d e"
+                                    "f f . . . . . g g"
+                                    `
+                                },
+                                childStyle:{
+                                    opacity: 0.5
+                                },
+                                childByIndexStyle:{
+                                    0: {gridArea:'a'},
+                                    1: {gridArea: '2 / 6 / 3 / 7'},
+                                    2: {gridArea:'b'},
+                                    3: {gridArea:'c'},
+                                    4: {gridArea:'d'},
+                                    5: {gridArea:'e'},
+                                    6: {gridArea:'f'},
+                                    7: {gridArea:'g'},
+                                    8: {gridArea:'1 / 1 / 3 / 4', border: 'solid medium green'},
+                                    9: {gridArea: 'a / b'}
+                                    // 9: {gridArea: '2 / 3 / 4 / 5', border: 'solid medium red'},
+                                }
+                            }, 
+                            {
+                                elementType:'Paragraph',
+                                content:`
+
+                                `
+                            },
+
+
+
+
+                        ]
+                    }
+            },
+
+
+
+
+
+            
+
+
+
+
+        ] // items
+    } // content
+} //gridTemplateColumnContent
+
+
+
+
+
+export default function getGridTemplateAreaContent(){
+return gridTemplateAreaContent;
+}
