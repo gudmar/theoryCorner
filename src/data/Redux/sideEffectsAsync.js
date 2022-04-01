1570af8afb21e038ba2edce67254a96f13e32f30
Marek Gudalewicz <marek.gudalewicz@localhost>
2022-04-01 23:35:11 +0200

    Siple explanation added to the enhancers and middleware in the Redux

--- src/data/Redux/sideEffectsAsync.js
+++ src/data/Redux/sideEffectsAsync.js
@@ -31,11 +31,77 @@ let data =     {
                     content: [
                         {id: 'sideEffects', title: 'sideEffects'},
                         {id: 'middleware', title: 'Middleware'},
+                        {id: 'enhancers', title: 'Enhancers'},
                         {id: 'AsyncAndPromises', title: 'Async and promises'},
                         {id: 'gettingState', title: 'gettingState'},
+                        // Middleware adds extra functionality to the Redux dispatch, 
+                        // Enhancser adds extra functionality to the redux store
+                        // https://stackoverflow.com/questions/67118933/redux-enhancer-example !!
 
                     ]
                 },
+
+
+                {
+                    elementType: 'Headline-3',
+                    content: `<span id="middleware">Middleware</span>`
+                },
+                {
+                    elementType: 'UnsignedList',
+                    content: [
+                        `Middleware extend <code>dispatch</code> functionality.`
+                    ]
+                },
+                {
+                    elementType: 'Code',
+                    content: `
+<pre>
+const loggerMiddleware = storeAPI => next => action => {
+    console.log('dispatching', action)
+    let result = next(action)
+    console.log('next state', storeAPI.getState())
+    return result
+  }
+</pre>                    
+                    `
+                },
+
+
+
+
+                {
+                    elementType: 'Headline-3',
+                    content: `<span id="enhancers">Enhancers</span>`
+                },
+                {
+                    elementType: 'UnsignedList',
+                    content: [
+                        `Enhancer takes a <code>createStore</code> function, and returns a new function that creates the store and does something more.
+                        In other words it works similar to the decorator pattern, where <code>createStore</code> is wrapped in a function,`,
+                        `Enhancers extend store, state logic,`,
+                        `In particular there is the <code>applyMiddleware</code> enhancer, that is capable of adding extre functionalities to the 
+                        <code>dispatch</code> method, and is used to apply middleware in the <code>createStore</code> function.`
+                    ]
+                },
+                
+                {
+                    elementType: 'Code',
+                    content: `
+<pre>
+const ourAwesomeEnhancer = createStore => (reducer, initialState, enhancer) => {
+    const store = createStore(monitoredReducer, initialState, enhancer);
+    //  enhancer logic
+  
+    return {
+      ...store
+      //   override the some store properties or add new ones
+    };
+  };
+</pre>                    
+                    `
+                },
+
+
 
             ]
         },
@@ -60,6 +126,12 @@ let data =     {
                 {
                     elementType:'Headline-2',
                     content:'References'
+                },
+                {
+                    elementType:'Link',
+                    content:'stackoverflow',
+                    href: 'https://stackoverflow.com/questions/67118933/redux-enhancer-example',
+                    description:'Example of an enhancer'
                 },
                 {
                     elementType:'Link',
